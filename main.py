6d4d398517da8dfe2c669dd24574eaa5753e4a1c
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-07-15 21:58:06 +0200

    Split off argument parsing to separate file

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-import argparse
 import logging
 import os
 import shutil
-import sys
 
-import config
-from fk_exceptions import AppError, SkippableError
+from argparser import ArgumentParser
+from fk_exceptions import SkippableError
 from interactive import update_existing_file
 from notify_runner import run_inotify, handle_file
 
@@ -41,80 +39,8 @@ def run(watch_dir, move_to_dir):
 
 
 def main():
-    top_parser = argparse.ArgumentParser(
-        description="Moving frikanalen videos and processing media files"
-    )
-    subparsers = top_parser.add_subparsers()
-
-    common_args = argparse.ArgumentParser(add_help=False)
-    common_args.add_argument(
-        "--indir",
-        help="""Folder where new `<id>/mediafile.mp4` are found
-            (default: %(default)s)""",
-        default=DIR,
-    )
-    common_args.add_argument(
-        "--outdir",
-        help="""Folder where `<id>/<media_folders>/<file>` are processed
-            (default: %(default)s)""",
-        default=TO_DIR,
-    )
-    common_args.add_argument(
-        "--no-api", action="store_true", help="Do not call the API"
-    )
-    common_args.add_argument(
-        "--api",
-        help="Frikanalen API url (default: %(default)s, env: FK_API)",
-        default=FK_API,
-    )
-    common_args.add_argument(
-        "--token",
-        help="Frikanalen API token to update database (env: FK_TOKEN)",
-        default=FK_TOKEN,
-    )
-
-    process_p = subparsers.add_parser(
-        "process",
-        parents=[common_args],
-        help="Reprocess media files (manual try-again mode)",
-    )
-    process_p.add_argument(
-        "video_id", type=int, help="The ID of the video you want to update (ie. 626060)"
-    )
-    process_p.add_argument(
-        "-f",
-        "--force",
-        action="store_true",
-        help="Overwrite (rm) existing media files instead of skipping",
-    )
-    process_p.set_defaults(cmd=process_cmd)
-
-    run_p = subparsers.add_parser(
-        "run",
-        parents=[common_args],
-        help="Check files in in-folder and process files to out-folder",
-    )
-    run_p.set_defaults(cmd=run_cmd)
-
-    daemon_p = subparsers.add_parser(
-        "daemon",
-        parents=[common_args],
-        help="Listen for new folders in in-folder and process files to out-folder",
-    )
-    daemon_p.set_defaults(cmd=daemon_cmd)
-
-    config.args = top_parser.parse_args()
-    if "cmd" not in config.args:
-        top_parser.print_help()
-        sys.exit(1)
-    try:
-        config.args.cmd(config.args)
-    except KeyboardInterrupt:
-        pass
-    except AppError as e:
-        print()
-        print(e)
-        sys.exit(1)
+    parser = ArgumentParser()
+    parser.parse_and_execute()
 
 
 def process_cmd(args):
