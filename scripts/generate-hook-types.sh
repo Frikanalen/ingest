4b2051bd03949ad31866a0a7f030810b7a7d5555
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-07-23 19:42:20 +0200

    codegen: update generate-hook-types.sh
    * tweak debug messages
    * add --force-optional so all properties are marked as optional
    (we have to do it this way because somewhere along the way PartialUploads ceases to be optional.)

--- scripts/generate-hook-types.sh
+++ scripts/generate-hook-types.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 set -euo pipefail
 
-cd scripts/ || echo
+cd scripts/ 2> /dev/null || echo>>/dev/null
 
-SCHEMA_JSON="hook.schema.json"
+SCHEMA_JSON=$PWD"/hook.schema.json"
 PYTHON_OUT="../libraries/hookschema.py"
 
-# 1. Compile and run Go program to generate JSON Schema
+echo " - Compiling and running Go program to generate JSON Schema..."
 go run gen_hook_schema.go > "$SCHEMA_JSON"
+echo " ✅ JSON Schema stored to ${SCHEMA_JSON}."
 
-# 2. Convert to Pydantic using datamodel-code-generator
+
+echo " - Converting to Pydantic using datamodel-code-generator..."
 uv run datamodel-codegen \
     --formatters isort ruff-check ruff-format \
     --input "$SCHEMA_JSON" \
+    --force-optional \
     --snake-case-field \
     --input-file-type jsonschema \
     --output "$PYTHON_OUT" \
     --output-model-type pydantic_v2.BaseModel
 
-echo "✅ Python types generated in $PYTHON_OUT"
+echo " ✅ Python types generated in $PYTHON_OUT"
