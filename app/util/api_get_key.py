8be4cd9a76ce556e1e06e78b9a293eee219bf0d8
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-08-01 01:50:52 +0200

    fix: adapt to new/old token endpoint

--- app/util/api_get_key.py
+++ app/util/api_get_key.py
@@ -1,7 +1,7 @@
 import logging
 
 from frikanalen_django_api_client import Client
-from frikanalen_django_api_client.api.obtain_token_v2 import obtain_token_v2_create
+from frikanalen_django_api_client.api.obtain_token import obtain_token_create
 from frikanalen_django_api_client.models import AuthTokenRequest
 
 logger = logging.getLogger(__name__)
@@ -14,5 +14,5 @@ def api_get_key(api_url: str, username: str, password: str) -> str:
     """
     logger.info("Obtaining API key for user: %s", username)
     login_client = Client(raise_on_unexpected_status=True, follow_redirects=True, base_url=str(api_url))
-    response = obtain_token_v2_create.sync_detailed(body=(AuthTokenRequest(username, password)), client=login_client)
+    response = obtain_token_create.sync_detailed(body=(AuthTokenRequest(username, password)), client=login_client)
     return response.parsed.token
